ef74eee73215d2b05eaebc6571c1331611efd8f8
142433 <142433@ust-global.com>
2023-02-02 21:50:04 +0530

    ATM MACHINE APP

---
+++ Library management System/src/BookServiceInterf.java
@@ -0,0 +1,12 @@
+public interface BookServiceInterf {
+    void addBook();
+
+    void showAllBooks();
+
+    void showAllAvailableBooks();
+
+    void borrowBook();
+
+    void returnBook();
+}
+
---
+++ src/COM/ATM/AtmOperationInterf.java
@@ -0,0 +1,7 @@
+package COM.ATM;
+
+public interface AtmOperationInterf {  public void viewBalance();
+    public void withdrawAmount(double withdrawAmount);
+    public void depositAmount(double depositAmount );
+    public  void viewMiniStatement();
+}
